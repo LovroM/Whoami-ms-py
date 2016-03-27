e5889eda19c1a48ec8d98cb1b4c48459ba2f6f09
Lovro <mekis.lovro@gmail.com>
2016-03-27 18:20:47 +0000

    detect ip 1/3

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify
+from socket import gethostname, gethostbyname 
 
 app = Flask(__name__)
 
@@ -9,8 +10,9 @@ def index():
 @app.route("/api/whoami")
 def who_is():
     
+    ip = gethostbyname(gethostname())    
 
-    return jsonify(ipaddress = "62.42.167.183",language = "sl-SI", software = "Windows NT 10.0; Win64; x64")
+    return jsonify(ipaddress = ip, language = "def sl-SI", software = "def Windows NT 10.0; Win64; x64")
 
 
 ### Runs server
